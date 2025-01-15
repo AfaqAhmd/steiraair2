baf9f38bfeafe18eccd54f679eb5c1c1a6ae814e
Afaq Ahmd <afaqahmed468@gmail.com>
2025-01-15 18:10:30 +0500

    15-1-25

--- toggle.js
+++ toggle.js
@@ -5,3 +5,12 @@
   mobileMenuToggle.addEventListener("click", () => {
     mobileNav.classList.toggle("active");
   });
+
+
+  document.querySelectorAll('nav ul li a').forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add('active');
+    } else {
+        link.classList.remove('active');
+    }
+});
